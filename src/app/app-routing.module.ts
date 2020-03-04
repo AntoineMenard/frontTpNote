3ec907b61659564ed1fb74895e4b9a6e5aa750f5
AntoineMenard <antoine.menard222@gmail.com>
2020-03-04 10:17:05 +0100

    Début projet note

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { InscriptionComponent } from './inscription/inscription.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path : 'inscription', component : InscriptionComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule { 
+
+}
