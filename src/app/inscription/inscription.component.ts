b3b801d203feda5652590e965c86b77bb524ceaa
AntoineMenard <antoine.menard222@gmail.com>
2020-03-04 14:59:13 +0100

    inscription et retour (sur note pour le moment)

--- src/app/inscription/inscription.component.ts
+++ src/app/inscription/inscription.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Person } from '../Model/Person';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-inscription',
@@ -10,7 +11,7 @@ import { Person } from '../Model/Person';
 export class InscriptionComponent implements OnInit {
 
   person: Person = new Person();
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient, private router: Router) { }
 
   ngOnInit(): void {
   }
@@ -18,11 +19,17 @@ export class InscriptionComponent implements OnInit {
   addPerson() {
     this.http.post('http://localhost:8088/createperson', this.person)
     .subscribe(data => {
+     
+
 
     }, err => {
       console.log(err);
     });
+    this.router.navigate(['/note']);
+  }
 
+  goToConnection() {
+    this.router.navigate(['/note']);
   }
 
 }
